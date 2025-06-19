0afa6f6c59671f680f922ee5e754822e91fd9611
didgmltmd <gomboy03@gmail.com>
2025-06-19 22:24:00 +0900

    fix:캐릭터명 수정

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -13,7 +13,7 @@ const CHARACTER_LIST = [
   { name: "양희승", character: "이브까지" },
   { name: "장기호", character: "시그하는애" },
   { name: "김도훈", character: "쟌뱃" },
-  { name: "최정재", character: "Eunweoll" },
+  { name: "최정재", character: "드럼통특공대" },
   { name: "손규열", character: "렬활" },
   { name: "홍현우", character: "BiiArkra" },
 ];
